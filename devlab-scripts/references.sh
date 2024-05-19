5f3e26f5b7388fecbf997c67cf849d2b5c096fd2
ROOTFOO <rootfoo@dev2deploy.io>
2024-05-19 16:49:42 -0400

    Create references.sh

---
+++ devlab-scripts/references.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+cd "${scriptDir}/.." || exit 1
+base=$(pwd)
+
+if [ $# -ne 0 ]; then
+
+  for repo in "$@"; do
+    path=$(dirname $repo)
+    git clone --single-branch https://github.com/$repo $base/references/$path
+  done
+
+else
+
+  for repo in $base/references/*; do
+    git -C $repo pull --rebase
+  done
+
+fi
